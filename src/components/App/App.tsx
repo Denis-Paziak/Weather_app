a6bbdbcb9e47d9fc0cb23a960bf87587b156679b
Denis-Paziak <denispaziak@gmail.com>
2023-06-20 11:30:00 +0300

    Time list

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ListDays from "../DayList/ListDays";
 import DayData from "../DayData/DayData";
+import TimeList from "../TimeList/TimeList";
 
 const App = () => {
     return (
         <div>
             <ListDays />
             <DayData />
+            <TimeList />
         </div>
     );
 };
